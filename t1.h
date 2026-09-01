92dfedc1a6009b82b50dbe0ea63e5b923504b291
888666444 <95750947+hsdgekwjrf@users.noreply.github.com>
2026-09-01 20:50:04 +0800

    idk
    idk

--- t1.h
+++ t1.h
@@ -1,12 +1,10 @@
+#include <iostream>
 #include <cmath>
 #include <random>
 #include <ctime>
 
 struct model{
-	std::default_random_engine seed(static_cast<unsigned>(time(nullptr)));
-	std::uniform_real_distribution<double> f_random(0.0,1.0);
-	
-	double SELU(double x){
+	const double SELU(double x){
 		double a = 1.050700987;
 		double b = 1.673263242;
 		if(x>0){
@@ -18,9 +16,24 @@ struct model{
 	}
 	
 	double *i[10];
-	double f[10] = {0,f_random(seed),f_random(seed),f_random(seed),f_random(seed),f_random(seed),f_random(seed),f_random(seed),f_random(seed),f_random(seed)};
+	double f[10] = {0,0,0,0,0,0,0,0,0,0};
 	double *o;
-	double fo = f_random(seed);
+	double fo = 0;
+	void init(){
+		std::random_device seed;
+		std::mt19937 gen(seed());
+		std::uniform_real_distribution<double> f_random(0.0,1.0);
+		f[1] = f_random(gen);
+		f[2] = f_random(gen);
+		f[3] = f_random(gen);
+		f[4] = f_random(gen);
+		f[5] = f_random(gen);
+		f[6] = f_random(gen);
+		f[7] = f_random(gen);
+		f[8] = f_random(gen);
+		f[9] = f_random(gen);
+		fo = f_random(gen);
+	}
 	void r1(){
 		*o = SELU(fo + *i[1] * f[1] + *i[2] * f[2] + *i[3] * f[3] + *i[4] * f[4] + *i[5] * f[5] + *i[6] * f[6] + *i[7] * f[7] + *i[8] * f[8] + *i[9] * f[9]);
 	}
